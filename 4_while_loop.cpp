de9cd44c3eb4a6e61fceb50a9546164fcdae8d4c
Aman kumar <72993406+AMANJIKUMAR@users.noreply.github.com>
2022-02-28 20:58:24 +0530

    Add files via upload

---
+++ 4_while_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+int i=1;
+while (i<=10)    // intilization
+{
+cout<<i<<' ';    //task
+i=i+1 ;          //updation
+}
+cout<<endl;
+return 0;
+}
